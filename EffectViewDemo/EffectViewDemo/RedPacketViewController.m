5ab380f872e464379879fb15d123b3fae71f1052
wangdewei <wangdewei@cmstop.com>
2017-08-12 21:11:21 +0800

    完善

--- EffectViewDemo/EffectViewDemo/RedPacketViewController.m
+++ EffectViewDemo/EffectViewDemo/RedPacketViewController.m
@@ -6,8 +6,7 @@
 //  Copyright © 2016年 Arvin. All rights reserved.
 //
 
-#import "ViewController.h"
-#import "UIView+Extension.h"
+#import "RedPacketViewController.h"
 #import "UIImageView+LBBlurredImage.h"
 #import "Masonry.h"
 
@@ -17,13 +16,12 @@
 #define Factor_6(A) kWIDTH / 375.0 * (A)
 
 
-static NSString *const TestBURL = @"alipay://";
 
-@interface ViewController ()
+@interface RedPacketViewController ()
 
 @end
 
-@implementation ViewController
+@implementation RedPacketViewController
 
 
 - (void)viewDidLoad {
@@ -31,21 +29,20 @@ static NSString *const TestBURL = @"alipay://";
     [super viewDidLoad];
     
     UIImageView *bgImgView = [[UIImageView alloc] init];
-//    [bgImgView setFrame:self.view.bounds];
     [bgImgView setUserInteractionEnabled:YES];
     [bgImgView setContentMode:UIViewContentModeScaleAspectFill];
+    
+
 
     // 使用第三方工具类
-    [self blurredImage:bgImgView];
+    [self blurredImage:bgImgView imageNamed:@"huba.jpeg"];
     
     // 头像
     UIImageView *headImgView = [[UIImageView alloc] init];
     [bgImgView addSubview:({
         [headImgView setUserInteractionEnabled:YES];
-//        [headImgView setFrame:(CGRect){bgImgView.centerX-(kWIDTH/1.28)/2, bgImgView.centerY-(kWIDTH/1.28)*0.8, kWIDTH/1.28, (kWIDTH/1.28)*1.18}];
         [headImgView setImage:[UIImage imageNamed:@"huba.jpeg"]];
         [headImgView setContentMode:UIViewContentModeScaleAspectFill];
-//        [headImgView.layer setCornerRadius:CGRectGetHeight(headImgView.frame)*0.5];
         [headImgView.layer setMasksToBounds:YES];
         [headImgView.layer setBorderWidth:6.f];
         [headImgView.layer setBorderColor:RGB(170, 170, 170, 0.5).CGColor];
@@ -65,16 +62,12 @@ static NSString *const TestBURL = @"alipay://";
         make.height.mas_equalTo(Factor_6(310));
     }];
     
-    
-    
     //关闭
     UIImageView *closeImgView = [[UIImageView alloc] init];
     [bgImgView addSubview:({
         [closeImgView setUserInteractionEnabled:YES];
-//        [closeImgView setFrame:(CGRect){headImgView.x-35, headImgView.y+headImgView.frame.size.height+120, bgImgView.size.width/2-10, 50}];
         [closeImgView setImage:[UIImage imageNamed:@"a_close.png"]];
         [closeImgView setContentMode:UIViewContentModeScaleAspectFill];
-        //        [headImgView.layer setCornerRadius:CGRectGetHeight(headImgView.frame)*0.5];
         [closeImgView.layer setMasksToBounds:YES];
         [closeImgView.layer setBorderWidth:6.f];
         [closeImgView.layer setBorderColor:RGB(170, 170, 170, 0.5).CGColor];
@@ -82,7 +75,6 @@ static NSString *const TestBURL = @"alipay://";
                                            initWithTarget:self action:@selector(closeImgViewClick:)]];
         closeImgView;
     })];
-    
     
     [closeImgView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(bgImgView.mas_left).mas_offset(Factor_6(35));
@@ -96,10 +88,8 @@ static NSString *const TestBURL = @"alipay://";
     UIImageView *alipayImgView = [[UIImageView alloc] init];
     [bgImgView addSubview:({
         [alipayImgView setUserInteractionEnabled:YES];
-        [alipayImgView setFrame:(CGRect){closeImgView.x+closeImgView.size.width+5, headImgView.y+headImgView.frame.size.height+120, bgImgView.size.width/2-10, 50}];
         [alipayImgView setImage:[UIImage imageNamed:@"a_alipay.png"]];
         [alipayImgView setContentMode:UIViewContentModeScaleAspectFill];
-        //        [headImgView.layer setCornerRadius:CGRectGetHeight(headImgView.frame)*0.5];
         [alipayImgView.layer setMasksToBounds:YES];
         [alipayImgView.layer setBorderWidth:6.f];
         [alipayImgView.layer setBorderColor:RGB(170, 170, 170, 0.5).CGColor];
@@ -115,37 +105,16 @@ static NSString *const TestBURL = @"alipay://";
         make.height.mas_equalTo(Factor_6(50));
         
     }];
-    
-    
-    
-    // 签名
-//    [bgImgView addSubview:({
-//        UILabel *lable = [[UILabel alloc] init];
-//        [lable setText:@"我是测试签名"];
-//        [lable setTextColor:[UIColor greenColor]];
-//        [lable setTextAlignment:NSTextAlignmentCenter];
-//        [lable setFont:[UIFont systemFontOfSize:FONT_SIZE]];
-//        [lable setBackgroundColor:[UIColor orangeColor]];
-//        CGSize lableSize = [lable.text boundingRectWithSize:CGSizeMake(MAXFLOAT,MAXFLOAT) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:FONT_SIZE]} context:nil].size;
-//        [lable setFrame:CGRectMake((kWIDTH*0.5)-(lableSize.width*0.5),CGRectGetMaxY(headImgView.frame)+15, lableSize.width, lableSize.height)];
-//        [lable sizeToFit];
-//        lable;
-//    })];
+
 }
 
 
 
 
-- (void)blurredImage:(UIImageView *)bgImgView {
-    // 方法1,没有blurRadius参数,因为默认是20
-    //[bgImgView setImageToBlur:[UIImage imageNamed:@"huoying4.jpg"] completionBlock:nil];
-    
-    // 方法2,对背景图片进行毛玻璃效果处理,参数blurRadius默认是20,可指定,最后一个参数block回调可为nil
-    [bgImgView setImageToBlur:[UIImage imageNamed:@"huba.jpeg"] blurRadius:8 completionBlock:nil];
+- (void)blurredImage:(UIImageView *)bgImgView imageNamed:(NSString *)name {
+    // 对背景图片进行毛玻璃效果处理,参数blurRadius默认是20,可指定,最后一个参数block回调可为nil
+    [bgImgView setImageToBlur:[UIImage imageNamed:name] blurRadius:8 completionBlock:nil];
     [self.view addSubview:bgImgView];
-    
-//    CGRect frame = CGRectMake(0, bgImgView.h-80, kWIDTH, 30);
-//    [bgImgView addSubview:[self labelWithFrame:frame text:@"LBBlurredImage"]];
 }
 
 
@@ -164,18 +133,14 @@ static NSString *const TestBURL = @"alipay://";
 }
 
 -(void)closeImgViewClick:(UITapGestureRecognizer *)gesture {
-    NSLog(@"%@->close",gesture);
-    if ([[UIApplication sharedApplication] canOpenURL:[NSURL  URLWithString:@"mqq://"]]){
-        NSLog(@"install--");
-    }else{
-        NSLog(@"no---");
-    }
     
-   
+    NSLog(@"%@->close",gesture);
+    
 }
 
 -(void)alipayImgViewClick:(UITapGestureRecognizer *)gesture {
     
+    NSLog(@"dakai");
     NSURL *url = [NSURL URLWithString:@"alipay://"];
     BOOL result = [[UIApplication sharedApplication] canOpenURL:url];
     
